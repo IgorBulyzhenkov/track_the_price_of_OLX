8f12d5e0ffd285034d1c439520624485b3447f5a
Igor Bulyzhenkov <98158893+IgorBulyzhenkov@users.noreply.github.com>
2024-01-06 00:14:40 +0200

    Revert "regster & auth create"

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\Email\ConfirmEmailController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,22 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('guest')->group( function(){
-
-    Route::view('/register',                'auth.register')->name('register');
-    Route::view('/wait-confirm',            'confirm.wait')->name('wait');
-    Route::view('/login',                   'auth.login')->name('login');
-
-    Route::get('/confirm-email/{token}',        [ConfirmEmailController::class, 'index'])->name('confirm.email');
-
-    Route::post('/register/create',             [RegisterController::class, 'create'])->name('reg.create');
-    Route::post('/login/create',                [LoginController::class, 'create'])->name('login.create');
-
-});
-
-Route::middleware('auth')->group( function (){
-
-    Route::view('/',                        'home.index')->name('home');
-
-    Route::get('/logout',                       [LogoutController::class, 'logout'])->name('logout');
+Route::get('/', function () {
+    return view('welcome');
 });
